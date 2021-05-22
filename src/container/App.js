7a43796e5e92eb8214d0a13e29e7d3e15e2f5b68
Kushal Bogati <kushalbogati@Kushals-MacBook-Air.local>
2021-05-22 12:26:29 +0545

    changes with hooks

--- src/container/App.js
+++ src/container/App.js
@@ -1,53 +1,42 @@
-import React, {Component} from "react";
+import React, {useState,useEffect} from "react";
 import List from "../components/worklist";
 import Searchbox from '../components/Searchbox';
 import './App.css';
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../ErrorBoundary';
 
-class App extends Component{
-    constructor() {
-    super()
-    this.state = {
-        robot : [],
-        searchfield : ""
-    }
-    console.log("constructor")
-}
+function App () {
+ 
+  const [robot ,setRobot] = useState([]);
+  const [searchfield, setSearchfield] = useState('')
 
- componentDidMount() {
-       fetch('https://jsonplaceholder.typicode.com/users')
-       .then(response => response.json())
-       .then(nepal => this.setState({robot : nepal}))
-       console.log("componentDidMount")
-   }
+  useEffect(()=>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(robofriend => setRobot(robofriend))
+  })
 
-   
-
-    onSearchChange = (event) => { // event is non-other than "input" of "searchbox".
-    this.setState({searchfield: event.target.value})//setState updates the state.In this case setState is updating,
-                                                 //the searchfield with whatever we type or what event value is.
-        
-    }
-
-    render(){
-        const {robot , searchfield} = this.state; //destructuring.....
-        const filteredRobots = robot.filter((robo)=> {
+    
+       
+    const filteredRobots = robot.filter((robo)=> {
             return (robo.name.toLowerCase().includes(searchfield.toLowerCase()))//here includes()is comparing
         })                                                              //if the searchfield has the string == robo.name
-        console.log("render")                                           //if yes print that robot                     
-        return(
+                                                                        //if yes print that robot                     
+    return (robot === 0) ? <h1>loading please wait</h1> : 
+     
+        (
         <div className="tc">   
            <h1 className="f1">RoboFriends</h1>
-           <Searchbox searchChange = {this.onSearchChange}/>
+           <Searchbox searchChange = {(event)=>setSearchfield(event.target.value)}/>
            <Scroll>
             <ErrorBoundary>
               <List robopeople={filteredRobots} />
             </ErrorBoundary>  
            </Scroll>
         </div>
-            ) 
-    } 
+        ) 
+
+
 }
 
 export default App;
